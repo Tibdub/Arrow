764c51c5b96622dbaf16ec98aae9e1eeb6eb238e
ThibaultDub <38186032+Tibdub@users.noreply.github.com>
2022-09-15 17:06:22 +0200

    Refactoring player movment script

--- Assets/_Projet Arrow/Scripts/MovePlayer.cs
+++ Assets/_Projet Arrow/Scripts/MovePlayer.cs
@@ -4,14 +4,49 @@ using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-
+    [Header("Deplacement")]
     public Rigidbody rb;
+    public Transform playerTransform;
+    public Transform cursorTransform;
     public float moveSpeed;
+    private float vertInput;
+    private float horzInput;
+
+
+    [SerializeField]
+    private Camera mainCamera;
+    private Vector3 mousePosition;
+
+
+    private void Update()
+    {
+        vertInput = Input.GetAxis("Vertical") * moveSpeed;
+        horzInput = Input.GetAxis("Horizontal") * moveSpeed;
+
+        PlayerRotation();
+    }
+
 
     void FixedUpdate()
     {
-        float vertSpeed = Input.GetAxis("Vertical") * moveSpeed;
-        float horzSpeed = Input.GetAxis("Horizontal") * moveSpeed;
-        rb.velocity = (Vector3.forward * vertSpeed) + (Vector3.right * horzSpeed);
+        rb.velocity = (Vector3.forward * vertInput) + (Vector3.right * horzInput);
+    }
+
+    public void PlayerRotation()
+    {
+        // Récupération de la souris % à la scène
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit))
+        {
+            mousePosition = raycastHit.point;
+        }
+
+        // Rotation du joueur vers le curseur
+        Vector3 lookDir = playerTransform.position - mousePosition;
+        float angle = Mathf.Atan2(lookDir.z, lookDir.x) * Mathf.Rad2Deg + 90f;
+        playerTransform.rotation = Quaternion.Euler(0f, -angle, 0f);
+
+        // Affichage du curseur
+        cursorTransform.position = mousePosition;
     }
 }
